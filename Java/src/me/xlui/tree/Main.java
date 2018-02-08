a31e4057d5cc2723a5fecc1b4399bc6b6c51a802
xlui <liuqi0315@gmail.com>
2018-02-08 15:00:53 +0800

    tree -- AVL Tree

--- Java/src/me/xlui/tree/Main.java
+++ Java/src/me/xlui/tree/Main.java
@@ -1,40 +1,32 @@
 package me.xlui.tree;
 
-import me.xlui.tree.impl.BinarySearchTree;
+import me.xlui.tree.impl.AVLTree;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		//	树结构：
-		//		30
-		//	  15  41
-		//	     33 50
-		BinarySearchTree tree = new BinarySearchTree();
-		tree.inOrderTraversal();
+		AVLTree tree = new AVLTree();
 
-		System.out.println("\n判断树中是否有 43 和 30：");
-		System.out.println(tree.hasElement(43));
-		System.out.println(tree.hasElementIter(30));
-		System.out.println("查找树中最大值：");
-		System.out.println(tree.findMax());
-		System.out.println("查找树中最小值：");
-		System.out.println(tree.findMin());
+		tree.insert(3);
+		tree.insert(2);
+		tree.insert(1);
+		tree.insert(4);
+		tree.insert(5);
+		tree.insert(6);
+		tree.insert(7);
+//		tree.insert(16);
+//		tree.insert(15);
+//		tree.insert(14);
+//		tree.insert(13);
+//		tree.insert(12);
+//		tree.insert(11);
+//		tree.insert(10);
+//		tree.insert(8);
+//		tree.insert(9);
 
-		System.out.println("插入元素 12：");
-		tree.insert(12);
-		tree.inOrderTraversal();
+		tree.remove(4);
 
-		System.out.println("\n删除元素 15：");
-		tree.delete(15);
-		tree.inOrderTraversal();
-		tree.insert(15);
-
-		System.out.println("\n删除元素 12：");
-		tree.delete(12);
-		tree.inOrderTraversal();
-		tree.insert(12);
-
-		System.out.println("\n删除元素 41：");
-		tree.delete(41);
+		tree.preOrderTraversal();
+		System.out.println();
 		tree.inOrderTraversal();
 	}
 }
