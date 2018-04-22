7416a43ce72835386863eaaf8020f41de0de2fca
xlui <liuqi0315@gmail.com>
2018-04-22 14:39:00 +0800

    BridgePattern

---
+++ Java/src/design_pattern/bridge/DrawAPI.java
@@ -0,0 +1,5 @@
+package design_pattern.bridge;
+
+public interface DrawAPI {
+	void drawCircle(int radius, int x, int y);
+}
---
+++ Java/src/design_pattern/bridge/GreenCircle.java
@@ -0,0 +1,8 @@
+package design_pattern.bridge;
+
+public class GreenCircle implements DrawAPI {
+	@Override
+	public void drawCircle(int radius, int x, int y) {
+		System.out.println("Drawing Circle[color: Green, radius: " + radius + ", x: " + x + ", y: " + y + "]");
+	}
+}
