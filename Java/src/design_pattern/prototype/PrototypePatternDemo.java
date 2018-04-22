62453b9972ae632b5b064ac2e57c45351c058087
xlui <liuqi0315@gmail.com>
2018-04-22 14:06:43 +0800

    PrototypePattern

---
+++ Java/src/design_pattern/prototype/PrototypePatternDemo.java
@@ -0,0 +1,16 @@
+package design_pattern.prototype;
+
+public class PrototypePatternDemo {
+	public static void main(String[] args) {
+		ShapeCache.loadCache();
+
+		Shape cloneShape1 = ShapeCache.getShape("1");
+		System.out.println("Shape: " + cloneShape1.getType());
+
+		Shape cloneShape2 = ShapeCache.getShape("2");
+		System.out.println("Shape: " + cloneShape2.getType());
+
+		Shape cloneShape3 = ShapeCache.getShape("3");
+		System.out.println("Shape: " + cloneShape3.getType());
+	}
+}
---
+++ Java/src/design_pattern/prototype/ShapeCache.java
@@ -0,0 +1,26 @@
+package design_pattern.prototype;
+
+import java.util.HashMap;
+
+public class ShapeCache {
+	private static HashMap<String, Shape> map = new HashMap<>();
+
+	public static Shape getShape(String shapeId) {
+		Shape shape = map.get(shapeId);
+		return (Shape) shape.clone();
+	}
+
+	public static void loadCache() {
+		Circle circle = new Circle();
+		circle.setId("1");
+		map.put(circle.getId(), circle);
+
+		Square square = new Square();
+		square.setId("2");
+		map.put(square.getId(), square);
+
+		Rectangle rectangle = new Rectangle();
+		rectangle.setId("3");
+		map.put(rectangle.getId(), rectangle);
+	}
+}
