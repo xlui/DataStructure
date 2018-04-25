1710a801fe928fc2e332c205c02b1ee643c55293
xlui <liuqi0315@gmail.com>
2018-04-25 19:46:42 +0800

    MediatorPattern

---
+++ Java/src/design_pattern/mediator/MediatorPatternDemo.java
@@ -0,0 +1,11 @@
+package design_pattern.mediator;
+
+public class MediatorPatternDemo {
+	public static void main(String[] args) {
+		User robert = new User("Robert");
+		User john = new User("john");
+
+		robert.sendMessage("Hi! John!");
+		john.sendMessage("Hello! Robert!");
+	}
+}
